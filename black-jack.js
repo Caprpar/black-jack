75bfa6b9a8f43802677a7ff9c8b747f7932a6dff
Caspar Danielsson <caspar.danielsson@iths.se>
2024-10-29 13:12:03 +0100

    add drawCard() addToHand() getHandValue()

--- black-jack.js
+++ black-jack.js
@@ -1,6 +1,6 @@
 /*
  * En kortlek har 52 kort med spader, hjärter, ruter, klöver: ess-kung
- * Kortleken ska blandas innan spelet börjar
+ * [x] Kortleken ska blandas innan spelet börjar
  * när korten delas ut, ska ett kort dras ur leken så antal kort minskas
  *
  * En dealer och en spelare tävlar om vem som närmast når 21 poäng,
@@ -13,24 +13,42 @@
  * 2-10 är värda sina poäng
  * knekt - kung är värda 10
  * ess är värt 1 eller 11 och anpassar sig till bästa möjliga hand
+ *
+ * [ ] Blandad kortlek
+ * [ ] Delarens hand
+ * [ ] Spelarens hand
+ * [ ] Få handvärde
+ * [ ] Välja om delaren ska dra ett kort eller inte
+ * [ ] Val att dra ett kort elle stanna
  */
-function getCard(cardValue, cardSuite, cardIndex) {
-  let card = [];
-  let cardName = `${cardValue}Of${cardSuite}`;
-  let isCardAce = cardValue === "ace" ? true : false;
-  let currentCardValue = isCardAce ? [1, 11] : cardIndex + 1;
 
-  card[`${cardName}`] = {
+/** Generates card
+ * @param {String} cardValue - card valueName "Ace", "One", "King"
+ * @param {String} cardSuite - card suite "hearts", "clubs"
+ * @param {Number} cardIndex - card value 1, 2, 10
+
+ * @example
+ * const card = getCard("king", "heart", 13)
+ * console.log(card) // output: kingOfheart: {value: 10, suite: 'heart', img: 'kingOfheart.png', name: 'king of heart', isAce: false}
+*/
+function getCard(cardValue, cardSuite, cardIndex) {
+  let card;
+  let cardName = `${cardValue}Of${cardSuite}`;
+  let currentCardValue = cardIndex; // make sure knight, queen and king gets cardvalue = 10
+
+  card = {
     value: currentCardValue,
     suite: cardSuite,
     img: `${cardName}.png`,
-    name: `${cardValue} of ${cardSuite}`,
-    isAce: isCardAce,
+    name: `${cardValue} Of ${cardSuite}`,
   };
 
   return card;
 }
 
+/**
+ * Generates a deck with 52 cards
+ */
 function getDeck() {
   let suites = ["hearth", "club", "spade", "diamond"];
   let values = [
@@ -52,12 +70,69 @@ function getDeck() {
   let deck = [];
   for (const suite of suites) {
     for (const [cardIndex, value] of values.entries()) {
-      deck.push(getCard(value, suite, cardIndex));
+      deck.push(getCard(value, suite, cardIndex + 1));
     }
   }
   return deck;
 }
 
+/**Returns shuffled deck from deck
+ * @param {Object} deck - Deck object
+ */
+function shuffleDeck(deck) {
+  let shuffled = [];
+  while (deck.length !== 0) {
+    let randomCardIdx = Math.floor(Math.random() * deck.length);
+    let randomCard = deck.splice(randomCardIdx, 1);
+    shuffled.push(randomCard);
+  }
+  return shuffled;
+}
+
+/** Returns top card from deck */
+function drawCard(deck) {
+  return deck.pop();
+}
+
+/** Add card to hand*/
+function addToHand(hand, card) {
+  hand.push(card);
+}
+
+/** Get value from current hand */
+function getHandValue(hand) {
+  // Addera alla kortens värden
+  // om något av korten är ess, kolla om totala värdet + 11 > 21, ge då ess värdet 1
+  let value = 0;
+  for (const currentCard of hand) {
+    card = currentCard[0];
+    console.log(card);
+    value += card.value;
+  }
+  return value;
+}
+
 // console.log(getCard("ace", "heart"));
-let deck = getDeck();
-console.log(deck);
+let deck = shuffleDeck(getDeck());
+let game = true;
+
+let player = {
+  hand: [drawCard(deck), drawCard(deck)],
+  hasAce: false,
+};
+
+let dealer = {
+  hand: [drawCard(deck), drawCard(deck)],
+  hasAce: false,
+};
+
+while (game) {
+  // Display dealer score
+  console.log(getHandValue(player.hand));
+  console.log(getHandValue(dealer.hand));
+  game = false;
+  // Display player score
+  // Spelare och dealer drar två kort var
+  // Spelare väljer om den vill dra ett nytt kort
+  // Delaer drar ett kort om dens poäng är under 16
+}
